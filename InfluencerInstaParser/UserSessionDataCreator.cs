e4cb752f0382144bd85c28c91dd2f82267c51415
ivan <vertvery@gmail.com>
2019-03-14 23:58:34 +0300

    User data creator added

---
+++ InfluencerInstaParser/UserSessionDataCreator.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Specialized;
+using System.Configuration;
+using InstagramApiSharp.Classes;
+
+namespace InfluencerInstaParser
+{
+    public class UserSessionDataCreator
+    {
+        
+        private static int _currentAccount = 0;
+        public static UserSessionData UserSessionDataFromConfigs()
+        {
+            var accountsSection = (NameValueCollection)ConfigurationManager.GetSection("accounts"); //TODO using set
+            var login = accountsSection.GetKey(_currentAccount);
+            var password = accountsSection.Get(login);
+            _currentAccount++;
+
+            return new UserSessionData()
+            {
+                UserName = login,
+                Password = password
+            };
+        }
+        
+        public static UserSessionData UserSessionDataFromConfigsLogin(string login)
+        {
+            var accountsSection = (NameValueCollection)ConfigurationManager.GetSection("accounts");
+            var password = accountsSection.Get(login);
+            if (password == null)
+            {
+                throw new Exception("There is no such login in configs"); //TODO add custom exception
+            }
+
+            return new UserSessionData()
+            {
+                UserName = login,
+                Password = password
+            };
+        }
+    }
+}
