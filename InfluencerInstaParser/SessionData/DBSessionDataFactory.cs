c7374c765444003ac4c3e075a38f20fc69e5e740
ivan <vertvery@gmail.com>
2019-03-15 15:03:17 +0300

    blank class

---
+++ InfluencerInstaParser/SessionData/DBSessionDataFactory.cs
@@ -0,0 +1,17 @@
+using InstagramApiSharp.Classes;
+
+namespace InfluencerInstaParser.SessionData
+{
+    public class DBSessionDataFactory : IUserSessionDataFactory
+    {
+        public UserSessionData MakeSessionData()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public UserSessionData MakeSessionDataFromLogin(string login)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
