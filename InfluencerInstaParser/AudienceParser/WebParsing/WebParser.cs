5c82e58d576ead5033a9d0a61c63161bdfdc98ae
ivan <vertvery@gmail.com>
2019-03-21 14:59:43 +0300

    Instagram Gis hash

--- InfluencerInstaParser/AudienceParser/WebParsing/WebParser.cs
+++ InfluencerInstaParser/AudienceParser/WebParsing/WebParser.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using InstagramApiSharp.API;
@@ -15,10 +18,19 @@ namespace InfluencerInstaParser.AudienceParser.WebParsing
 
         public string GetEndOfCursorOnFirstPage(string username)
         {
-            return Regex.Matches(HtmlPageDownloader.GetJsonStringFromUserPage(username), "\"has_next_page\":true,\"end_cursor\":\".{120}")[0].ToString()
+            return Regex.Matches(HtmlPageDownloader.GetJsonStringFromUserPage(username),
+                    "\"has_next_page\":true,\"end_cursor\":\".{120}")[0].ToString()
                 .Split(":")[2].Remove(0, 1);
         }
-        
-            
+
+        public string MakeInstagramGis(string rhxGis, int userId, int count, string endOfCursor)
+        {
+            var signatureParams = $"{rhxGis}:{{\"id\":\"{userId}\",\"first\":{count},\"after\":\"{endOfCursor}\"}}";
+            using (var md5 = MD5.Create())
+            {
+                var gis = md5.ComputeHash(Encoding.UTF8.GetBytes(signatureParams));
+                return Convert.ToBase64String(gis);
+            }
+        }
     }
 }
