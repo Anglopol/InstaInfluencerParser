e7c79a63fe31f5d6412256096f3ffdead288d0e4
ivan <vertvery@gmail.com>
2019-03-22 13:43:22 +0300

    fixes

--- InfluencerInstaParser/AudienceParser/WebParsing/WebProcessor.cs
+++ InfluencerInstaParser/AudienceParser/WebParsing/WebProcessor.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -9,15 +11,15 @@ namespace InfluencerInstaParser.AudienceParser.WebParsing
 {
     public class WebProcessor
     {
-        public string GetRhxGisParameter(string username)
+        public string GetRhxGisParameter(string pageContent)
         {
-            return Regex.Matches(PageDownloader.GetJsonStringFromUserPage(username), "rhx_gis.{3}[^\"]*")[0].ToString()
+            return Regex.Matches(pageContent, "rhx_gis.{3}[^\"]*")[0].ToString()
                 .Split(":")[1].Remove(0, 1);
         }
 
-        public string GetEndOfCursorOnFirstPage(string username)
+        public string GetEndOfCursorOnFirstPage(string pageContent)
         {
-            return Regex.Matches(PageDownloader.GetJsonStringFromUserPage(username),
+            return Regex.Matches(pageContent,
                     "\"has_next_page\":true,\"end_cursor.{3}[^\"]*")[0].ToString()
                 .Split(":")[2].Remove(0, 1);
         }
@@ -28,18 +30,19 @@ namespace InfluencerInstaParser.AudienceParser.WebParsing
             Console.WriteLine(signatureParams);
             return CalculateMD5Hash(signatureParams);
         }
-        
+
         public string CalculateMD5Hash(string input)
         {
             var md5 = MD5.Create();
             var inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
             var hash = md5.ComputeHash(inputBytes);
- 
+
             var sb = new StringBuilder();
             foreach (var character in hash)
             {
                 sb.Append(character.ToString("x2"));
             }
+
             return sb.ToString();
         }
 
@@ -50,7 +53,7 @@ namespace InfluencerInstaParser.AudienceParser.WebParsing
 
         public string GetQueryUrlForPosts(int userId, int count, string endOfCursor)
         {
-            const string defaultQueryUrl = 
+            const string defaultQueryUrl =
                 @"/graphql/query/?query_hash=f2405b236d85e8296cf30347c9f08c2a&variables=";
             var signatureUrlString = HttpUtility.UrlEncode(MakeSignatureString(userId, count, endOfCursor));
             return defaultQueryUrl + signatureUrlString;
