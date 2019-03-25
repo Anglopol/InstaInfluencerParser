55f25095fad995c6d760a87604161d3cb093dcf5
ivan <vertvery@gmail.com>
2019-03-25 11:15:28 +0300

    timeout

--- InfluencerInstaParser/AudienceParser/WebParsing/WebParser.cs
+++ InfluencerInstaParser/AudienceParser/WebParsing/WebParser.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using InstagramApiSharp.Classes.Models;
 
@@ -16,18 +17,22 @@ namespace InfluencerInstaParser.AudienceParser.WebParsing
             _queryRequester = new QueryRequester();
         }
 
-        public async Task<List<string>> GetAllPostsShortCodesFromUser(InstaUserInfo user)
+        public async Task<List<string>> GetPostsShortCodesFromUser(InstaUserInfo user, int countOfLoading = 10)
         {
             var userUrl = "/" + user.Username + "/";
+//            var userUrl = "/varlamov/";
             var userPageContent = await PageDownloader.GetPageContent(userUrl);
             var rhxGis = _webProcessor.GetRhxGisParameter(userPageContent);
             var resultList = _webProcessor.GetListOfShortCodesFromPageContent(userPageContent);
             if (!_webProcessor.HasNextPage(userPageContent)) return resultList;
             var jsonPage = await _queryRequester.GetJson(userPageContent, user.Pk, rhxGis);
             resultList.AddRange(_webProcessor.GetListOfShortCodesFromQueryContent(jsonPage));
-            while (_webProcessor.HasNextPage(jsonPage))
+            var count = 0;
+            while (_webProcessor.HasNextPage(jsonPage) && count < countOfLoading)
             {
+                count++;
                 var nextCursor = _webProcessor.GetEndOfCursorFromJson(jsonPage);
+                Thread.Sleep(400);
                 jsonPage = await _queryRequester.GetJson(user.Pk, rhxGis, nextCursor);
                 resultList.AddRange(_webProcessor.GetListOfShortCodesFromQueryContent(jsonPage));
             }
