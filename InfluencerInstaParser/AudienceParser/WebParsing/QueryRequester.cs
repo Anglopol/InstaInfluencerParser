b4ac7268ba74983fc127b25be2fca7f7dae5ecb4
ivan <vertvery@gmail.com>
2019-03-25 09:42:07 +0300

    feature

--- InfluencerInstaParser/AudienceParser/WebParsing/QueryRequester.cs
+++ InfluencerInstaParser/AudienceParser/WebParsing/QueryRequester.cs
@@ -5,10 +5,18 @@ namespace InfluencerInstaParser.AudienceParser.WebParsing
 {
     public class QueryRequester
     {
-        public async Task<JObject> GetJsonFromUserPage(string userPageContent, long userId, string rhxGis)
+        public async Task<JObject> GetJson(string userPageContent, long userId, string rhxGis)
         {
             var proc = new WebProcessor();
             var endOfCursor = proc.GetEndOfCursorOnFirstPage(userPageContent);
+            var instagramGis = proc.MakeInstagramGisForPosts(rhxGis, userId, 12, endOfCursor);
+            var queryUrl = proc.GetQueryUrlForPosts(userId, 12, endOfCursor);
+            return proc.GetObjectFromJsonString(await PageDownloader.GetPageContent(queryUrl, instagramGis));
+        }
+
+        public async Task<JObject> GetJson(long userId, string rhxGis, string endOfCursor)
+        {
+            var proc = new WebProcessor();
             var instagramGis = proc.MakeInstagramGisForPosts(rhxGis, userId, 12, endOfCursor);
             var queryUrl = proc.GetQueryUrlForPosts(userId, 12, endOfCursor);
             return proc.GetObjectFromJsonString(await PageDownloader.GetPageContent(queryUrl, instagramGis));
