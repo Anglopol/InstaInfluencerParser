cbd5b2535fff22c36e8ef1387ba36cf5606472eb
ivan <vertvery@gmail.com>
2019-03-29 16:31:57 +0300

    rewriting ParsingSet class

--- InfluencerInstaParser/AudienceParser/SingletonParsingSet.cs
+++ InfluencerInstaParser/AudienceParser/SingletonParsingSet.cs
@@ -1,18 +1,20 @@
-using System.Collections.Concurrent;
-using System.Linq;
+using System.Collections.Generic;
 
 namespace InfluencerInstaParser.AudienceParser
 {
     public class SingletonParsingSet
     {
         private static SingletonParsingSet _instance;
-        public ConcurrentDictionary<string, byte> HandledUsers { get; }
-        public ConcurrentQueue<string> UnprocessedUsers { get; }
+        
+        private Queue<string> ShortCodesQueue { get; }
+        private HashSet<string> UnprocessedUsers { get; }
+        private HashSet<string> ProcessedUsers { get; }
 
         private SingletonParsingSet()
         {
-            HandledUsers = new ConcurrentDictionary<string, byte>();
-            UnprocessedUsers = new ConcurrentQueue<string>();
+            ShortCodesQueue = new Queue<string>();
+            UnprocessedUsers = new HashSet<string>();
+            ProcessedUsers = new HashSet<string>();
         }
 
         public static SingletonParsingSet GetInstance()
@@ -20,39 +22,20 @@ namespace InfluencerInstaParser.AudienceParser
             return _instance ?? (_instance = new SingletonParsingSet());
         }
 
-        public void AddInQueue(string username)
+        public void AddUnprocessedUser(string username)
         {
-            if (!IsInQueue(username))
-            {
-                UnprocessedUsers.Enqueue(username);
-            }
+            if (ProcessedUsers.Contains(username)) return;
+            UnprocessedUsers.Add(username);
         }
 
-        public string Dequeue(string username)
+        public void AddProcessedUser(string username)
         {
-            UnprocessedUsers.TryDequeue(out var result);
-            return result;
+            ProcessedUsers.Add(username);
         }
 
-        public bool IsProcessed(string username)
+        public void AddInShortCodesQueue(string shortCode)
         {
-            return HandledUsers.ContainsKey(username);
-        }
-
-        public bool IsInQueue(string username) => UnprocessedUsers.Contains(username);
-
-
-        public bool IsInParsingSet(string username)
-        {
-            return IsProcessed(username) || IsInQueue(username);
-        }
-
-        public void AddInHandledSet(string username)
-        {
-            if (!IsProcessed(username))
-            {
-                HandledUsers.TryAdd(username, 1);
-            }
+            ShortCodesQueue.Enqueue(shortCode);
         }
     }
 }
