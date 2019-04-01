3ad24496b34f1f9458db1e56a70194ea3c6cc0b9
ivan <vertvery@gmail.com>
2019-04-01 17:40:01 +0300

    getting all user's active audience

--- InfluencerInstaParser/AudienceParser/ParsingHandler.cs
+++ InfluencerInstaParser/AudienceParser/ParsingHandler.cs
@@ -1,12 +1,46 @@
+using System;
+using System.Threading;
+using InfluencerInstaParser.AudienceParser.WebParsing;
+
 namespace InfluencerInstaParser.AudienceParser
 {
     public class ParsingHandler
     {
         private SingletonParsingSet _parsingSet;
+        private int _countOfThreads;
+        private string _targetAccount;
 
-        public ParsingHandler()
+        public ParsingHandler(string username)
         {
             _parsingSet = SingletonParsingSet.GetInstance();
+            _targetAccount = username;
+        }
+
+        public void Parse()
+        {
+            var agents = new UserAgentCreator();
+            var web = new WebParser(agents.GetUserAgent());
+            web.GetPostsShortCodesFromUser(_targetAccount);
+            Console.WriteLine("Short Codes downloaded");
+            using (var countdownEvent = new CountdownEvent(_parsingSet.ShortCodesQueue.Count * 2))
+            {
+                foreach (var shortcode in _parsingSet.ShortCodesQueue)
+                {
+                    ThreadPool.QueueUserWorkItem(obj =>
+                    {
+                        new WebParser(agents.GetUserAgent()).GetUsernamesFromPostLikes(shortcode);
+                        countdownEvent.Signal();
+                    });
+                    
+                    ThreadPool.QueueUserWorkItem(obj =>
+                    {
+                        new WebParser(agents.GetUserAgent()).GetUsernamesFromPostComments(shortcode);
+                        countdownEvent.Signal();
+                    });
+                }
+
+                countdownEvent.Wait();
+            }
         }
         
     }
