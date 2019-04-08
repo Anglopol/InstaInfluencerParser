ce4a5dd5a43c77798f4fdae354926754ddef01c3
ivan <vertvery@gmail.com>
2019-04-08 13:14:06 +0300

    Communication type

---
+++ InfluencerInstaParser/AudienceParser/CommunicationType.cs
@@ -0,0 +1,11 @@
+namespace InfluencerInstaParser.AudienceParser
+{
+    public enum CommunicationType
+    {
+        Target,
+        Following,
+        Liker,
+        Commentator,
+        Follower
+    }
+}
--- InfluencerInstaParser/AudienceParser/User.cs
+++ InfluencerInstaParser/AudienceParser/User.cs
@@ -1,20 +1,33 @@
+using Newtonsoft.Json;
+
 namespace InfluencerInstaParser.AudienceParser
 {
     public class User
     {
-        public User(string username, int likes, int comments, int following, int followers)
+        public User(string username = null, int likes = 0, int comments = 0, int following = 0, int followers = 0,
+            User from = null, CommunicationType communicationType = CommunicationType.Target)
         {
             Username = username;
             Likes = likes;
             Comments = comments;
             Following = following;
             Followers = followers;
+            From = from;
+            CommunicationType = communicationType;
         }
 
-        public string Username { get; }
-        public int Likes { get; }
-        public int Comments { get; }
-        public int Following { get; }
-        public int Followers { get; }
+        [JsonProperty("name")] public string Username { get; }
+
+        [JsonProperty("likes")] public int Likes { get; }
+
+        [JsonProperty("comments")] public int Comments { get; }
+
+        [JsonProperty("following")] public int Following { get; }
+
+        [JsonProperty("followers")] public int Followers { get; }
+
+        [JsonProperty("from")] public User From { get; }
+
+        [JsonProperty("communication")] public CommunicationType CommunicationType { get; }
     }
 }
