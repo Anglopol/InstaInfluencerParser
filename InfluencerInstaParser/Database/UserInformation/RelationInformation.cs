fb515966cf60b8fdd11d07b5cae0762780c7fbc7
anglopol <vertvery@gmail.com>
2019-04-09 18:39:19 +0300

    Relation Information class

---
+++ InfluencerInstaParser/Database/UserInformation/RelationInformation.cs
@@ -0,0 +1,54 @@
+using System;
+using InfluencerInstaParser.Database.User;
+using Newtonsoft.Json;
+
+namespace InfluencerInstaParser.Database.UserInformation
+{
+    public class RelationInformation
+    {
+        private readonly object _setCommentsLocker;
+        private readonly object _setLikesLocker;
+        private int _likes;
+        private int _comments;
+
+        [JsonProperty("communication")] public CommunicationType CommunicationType { get; }
+
+        [JsonProperty("likes")]
+        public int Likes
+        {
+            get => _likes;
+            set
+            {
+                if (value < 0) throw new ArgumentOutOfRangeException(nameof(value));
+                lock (_setLikesLocker)
+                {
+                    _likes = value;
+                }
+            }
+        }
+
+        [JsonProperty("comments")]
+        public int Comments
+        {
+            get => _comments;
+            set
+            {
+                if (value < 0) throw new ArgumentOutOfRangeException(nameof(value));
+                lock (_setCommentsLocker)
+                {
+                    _comments = value;
+                }
+            }
+        }
+
+        public User Parent { get; }
+
+        public RelationInformation(User parent, CommunicationType communicationType)
+        {
+            Parent = parent;
+            CommunicationType = communicationType;
+            _setLikesLocker = new object();
+            _setCommentsLocker = new object();
+        }
+    }
+}
