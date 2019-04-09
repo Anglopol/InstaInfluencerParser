a9523c0f35fe2bc69f973fa300baae4b2cb84d11
anglopol <vertvery@gmail.com>
2019-04-09 16:17:58 +0300

    refactoring

--- InfluencerInstaParser/Database/User.cs
+++ InfluencerInstaParser/Database/User.cs
@@ -1,9 +1,14 @@
+using System;
 using Newtonsoft.Json;
 
 namespace InfluencerInstaParser.Database
 {
     public class User
     {
+        private readonly object _setCommentsLocker;
+        private readonly object _setLikesLocker;
+        private int _comments;
+        private int _likes;
         public User(string username = null, int likes = 0, int comments = 0, int following = 0, int followers = 0,
             User from = null, CommunicationType communicationType = CommunicationType.Target)
         {
@@ -14,11 +19,40 @@ namespace InfluencerInstaParser.Database
             Followers = followers;
             From = from;
             CommunicationType = communicationType;
+            _setCommentsLocker = new object();
+            _setLikesLocker = new object();
         }
 
         [JsonProperty("name")] public string Username { get; }
-        [JsonProperty("likes")] public int Likes { get; set; }
-        [JsonProperty("comments")] public int Comments { get; set; }
+
+        [JsonProperty("likes")]
+        public int Likes
+        {
+            get => _likes;
+            set
+            {
+                if (value < 0) throw new ArgumentOutOfRangeException(nameof(value));
+                lock (_setLikesLocker)
+                {
+                    _likes = value;
+                }
+            }
+        }
+
+        [JsonProperty("comments")]
+        public int Comments
+        {
+            get => _comments;
+            set
+            {
+                if (value < 0) throw new ArgumentOutOfRangeException(nameof(value));
+                lock (_setCommentsLocker)
+                {
+                    _comments = value;
+                }
+            }
+        }
+
         [JsonProperty("following")] public int Following { get; set; }
         [JsonProperty("followers")] public int Followers { get; set; }
         [JsonProperty("from")] public User From { get; }
