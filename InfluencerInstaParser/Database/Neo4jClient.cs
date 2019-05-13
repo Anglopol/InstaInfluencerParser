0979565621bc5bc92f76f48f5d96179275df8f25
anglopol <vertvery@gmail.com>
2019-05-13 17:11:13 +0300

    refactoring

--- InfluencerInstaParser/Database/Neo4jClient.cs
+++ InfluencerInstaParser/Database/Neo4jClient.cs
@@ -29,7 +29,8 @@ namespace InfluencerInstaParser.Database
         {
             string[] queries =
             {
-                "CREATE INDEX ON :User(name)"
+                "CREATE INDEX ON :User(name)",
+                "CREATE INDEX ON :Location(name)"
             };
 
             using (var session = _driver.Session())
@@ -43,9 +44,12 @@ namespace InfluencerInstaParser.Database
             var modelUsers = (from user in users select user.ModelViewUser).ToList();
             var cypher = new StringBuilder()
                 .AppendLine("UNWIND {users} AS user")
+                .AppendLine("UNWIND user.parents AS parent")
                 .AppendLine(
-                    "MERGE (u:User {name: user.name, likes: user.likes, comments: user.comments, influencer: user.influencer, followers: user.followers, following: user.following})")
-                .AppendLine("SET u = user")
+                    "MERGE (u:User {name: user.name, following: user.following, followers: user.followers, locations: user.locations, influencer: user.influencer, parents: user.parents})")
+                .AppendLine("CASE user")
+                .AppendLine(
+                    "FOREACH (ignoreMe in CASE WHEN parent.name='@target' OR user.influencer=true THEN [1] ELSE [] END | SET u.likes=user.likes, u.comments=user.comments)")
                 .ToString();
 
             using (var session = _driver.Session())
@@ -59,7 +63,7 @@ namespace InfluencerInstaParser.Database
         {
             var cypher = new StringBuilder()
                 .AppendLine("UNWIND {locations} AS location")
-                .AppendLine("MERGE (l:Location {name: location.name, visitors: location.countOfUsers})")
+                .AppendLine("MERGE (l:Location)")
                 .AppendLine("SET l = location")
                 .ToString();
 
@@ -70,17 +74,18 @@ namespace InfluencerInstaParser.Database
             }
         }
 
-        public async Task CreateUsersRelationships(IList<User> users)
+        public async Task CreateUsersRelationships(IList<User> users, string targetName)
         {
             var relationsList = (from user in users from relation in user.Relations select relation.Value.Relation)
                 .ToList();
             var cypher = new StringBuilder()
                 .AppendLine("UNWIND {relations} AS relation")
-                // Find the User:
                 .AppendLine("MATCH (p:User { name: relation.parent })")
                 .AppendLine("MATCH (c:User { name: relation.child })")
+                .AppendLine("CASE p.name")
                 .AppendLine(
-                    "MERGE (c)-[:CHILD]->(r:RelationInformation {parent: relation.parent, child: relation.child, likes: relation.likes, comments: relation.comments})-[:PARENT]->(p)")
+                    $"FOREACH (ignoreMe in CASE WHEN p.name={targetName} THEN [1] ELSE [] END | SET c.likesToTarget=relation.likes, c.commentsToTarget=relation.comments)")
+                .AppendLine("MERGE (c)-[:CHILD]->(p)")
                 .ToString();
 
 
@@ -98,7 +103,6 @@ namespace InfluencerInstaParser.Database
                 .AppendLine("UNWIND {users} AS user")
                 .AppendLine("UNWIND user.locations AS location")
                 .AppendLine("UNWIND user.parents AS parent")
-                // Find the User:
                 .AppendLine("MATCH (u:User { name: user.name })")
                 .AppendLine("MATCH (l:Location { name: location, audienceFrom: parent })")
                 .AppendLine(
