93810229f044cb463c354439d191e32ef558d84a
anglopol <vertvery@gmail.com>
2019-06-03 12:12:42 +0300

    fixes

--- InfluencerInstaParser/AudienceParser/UserInformation/User.cs
+++ InfluencerInstaParser/AudienceParser/UserInformation/User.cs
@@ -218,7 +218,7 @@ namespace InfluencerInstaParser.AudienceParser.UserInformation
                     {
                         new Location
                         {
-                            Name = locationName, Owner = parentName, Id = cityId, CountOfUsers = 0
+                            Name = locationName, Owner = parentName, Id = cityId, CountOfUsers = 1
                         }
                     });
             }
