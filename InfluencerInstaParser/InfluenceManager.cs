1236cbe66c0a639eaa801733365fbd2dead55f02
anglopol <vertvery@gmail.com>
2019-06-04 18:56:53 +0300

    Перенос клиента в атрибуты класса

--- InfluencerInstaParser/InfluenceManager.cs
+++ InfluencerInstaParser/InfluenceManager.cs
@@ -12,48 +12,39 @@ namespace InfluencerInstaParser
 {
     public class InfluenceManager
     {
-        private readonly Uri _databaseConnectionUri;
-        private readonly string _databaseUsername;
-        private readonly string _databasePassword;
-        private readonly string _pathToProxyFile;
+        private readonly GraphClient _graphClient;
 
-        public InfluenceManager(Uri databaseConnectionUri, string databaseUsername, string databasePassword,
-            string pathToProxyFile)
+        public InfluenceManager(Uri databaseConnectionUri, string databaseUsername, string databasePassword)
         {
-            _databaseConnectionUri =
-                databaseConnectionUri ?? throw new ArgumentNullException(nameof(databaseConnectionUri));
-            _databaseUsername = databaseUsername ?? throw new ArgumentNullException(nameof(databaseUsername));
-            _databasePassword = databasePassword ?? throw new ArgumentNullException(nameof(databasePassword));
-            _pathToProxyFile = pathToProxyFile ?? throw new ArgumentNullException(nameof(pathToProxyFile));
+            _graphClient = new GraphClient(databaseConnectionUri, databaseUsername, databasePassword);
         }
 
-        public void ProcessUser(string targetUsername)
+        public void ProcessUser(string targetUsername, string pathToProxyFile)
         {
-            var client = new GraphClient(_databaseConnectionUri, _databaseUsername, _databasePassword);
-            client.Connect();
-            ProxyFromFileCreatorSingleton.GetInstance().SetPathToProxyFile(_pathToProxyFile);
+            _graphClient.Connect();
+            ProxyFromFileCreatorSingleton.GetInstance().SetPathToProxyFile(pathToProxyFile);
             var parser = new ParsingHandler(targetUsername);
             parser.Parse();
             var locations = ParsingSetSingleton.GetInstance().GetListOfLocations();
             var users = ParsingSetSingleton.GetInstance().GetProcessedUsers();
-            FillDatabase(client, users, locations);
+            FillDatabase(users, locations);
         }
 
-        private static void FillDatabase(GraphClient client, IEnumerable<User> users, IEnumerable<Location> locations)
+        private void FillDatabase(IEnumerable<User> users, IEnumerable<Location> locations)
         {
-            var enumerable = users.ToList();
-            var modelUsers = (from user in enumerable select user.ModelViewUser).ToList();
-            Neo4jClientHandler.CreateUsers(client, modelUsers);
-            Neo4jClientHandler.CreateLocations(client, locations);
+            var usersList = users.ToList();
+            var modelUsers = (from user in usersList select user.ModelViewUser).ToList();
+            Neo4jClientHandler.CreateUsers(_graphClient, modelUsers);
+            Neo4jClientHandler.CreateLocations(_graphClient, locations);
             var usersRelations =
-                (from user in enumerable from userDict in user.Relations select userDict.Value).ToList();
+                (from user in usersList from userDict in user.Relations select userDict.Value).ToList();
             var usersModelRelations = (from relation in usersRelations select relation.Relation).ToList();
-            Neo4jClientHandler.CreateUsersRelations(client, usersModelRelations);
+            Neo4jClientHandler.CreateUsersRelations(_graphClient, usersModelRelations);
             var locationsRelationsDict =
-                from user in enumerable from locationDict in user.Locations select locationDict.Value;
+                from user in usersList from locationDict in user.Locations select locationDict.Value;
             var locationRelation =
                 (from relation in locationsRelationsDict from values in relation.Values select values).ToList();
-            Neo4jClientHandler.CreateLocationsRelations(client, locationRelation);
+            Neo4jClientHandler.CreateLocationsRelations(_graphClient, locationRelation);
         }
     }
 }
