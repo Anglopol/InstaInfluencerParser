bcd160c92a696512c22888bcec2a4eaeac4d96ea
anglopol <vertvery@gmail.com>
2019-06-05 19:59:51 +0300

    Изменения главного класса (использование даты)

--- InfluencerInstaParser/InfluenceManager.cs
+++ InfluencerInstaParser/InfluenceManager.cs
@@ -13,25 +13,41 @@ namespace InfluencerInstaParser
     public class InfluenceManager
     {
         private readonly GraphClient _graphClient;
+        private readonly string _targetUsername;
+        private readonly DateTime _currentDate;
 
-        public InfluenceManager(Uri databaseConnectionUri, string databaseUsername, string databasePassword)
+        public InfluenceManager(Uri databaseConnectionUri, string databaseUsername, string databasePassword,
+            string targetUsername)
         {
+            _targetUsername = targetUsername;
             _graphClient = new GraphClient(databaseConnectionUri, databaseUsername, databasePassword);
+            _currentDate = DateTime.UtcNow;
         }
 
-        public void ProcessUser(string targetUsername, string pathToProxyFile)
+        public void ProcessUser(string pathToProxyFile)
         {
             _graphClient.Connect();
             ProxyFromFileCreatorSingleton.GetInstance().SetPathToProxyFile(pathToProxyFile);
-            var parser = new ParsingHandler(targetUsername);
+            var parser = new ParsingHandler(_targetUsername, _currentDate);
             parser.Parse();
             var locations = ParsingSetSingleton.GetInstance().GetListOfLocations();
             var users = ParsingSetSingleton.GetInstance().GetProcessedUsers();
             FillDatabase(users, locations);
         }
 
+        public List<ModelUser> GetListOfInfluencers(DateTime dateOfParsing)
+        {
+            return Neo4jClientHandler.GetListOfInfluencers(_graphClient, dateOfParsing, _targetUsername);
+        }
+
+        public List<Location> GetListOfLocationsFromTarget(DateTime dateOfParsing)
+        {
+            return Neo4jClientHandler.GetListOfLocationsFromTarget(_graphClient, dateOfParsing, _targetUsername);
+        }
+
         private void FillDatabase(IEnumerable<User> users, IEnumerable<Location> locations)
         {
+            _graphClient.Connect();
             var usersList = users.ToList();
             var modelUsers = (from user in usersList select user.ModelViewUser).ToList();
             Neo4jClientHandler.CreateUsers(_graphClient, modelUsers);
