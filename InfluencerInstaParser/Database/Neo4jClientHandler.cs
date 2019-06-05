639416a44b57f9a22712f696e12e619395d3415b
anglopol <vertvery@gmail.com>
2019-06-05 19:54:58 +0300

    Изменение класса хэндлер, вызванные добавлением информации о дате последней обработки пользователя

--- InfluencerInstaParser/Database/Neo4jClientHandler.cs
+++ InfluencerInstaParser/Database/Neo4jClientHandler.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using InfluencerInstaParser.Database.ModelView;
 using Neo4jClient;
@@ -35,10 +37,12 @@ namespace InfluencerInstaParser.Database
             {
                 graphClient.Cypher
                     .Match("(parent:User)", "(child:User)")
-                    .Where((ModelUser child) => child.Username == relation.Child)
-                    .AndWhere((ModelUser parent) => parent.Username == relation.Parent)
+                    .Where((ModelUser child) =>
+                        child.Username == relation.Child && child.DateOfParsing == relation.DateOfParsing)
+                    .AndWhere((ModelUser parent) =>
+                        parent.Username == relation.Parent && parent.DateOfParsing == relation.DateOfParsing)
                     .Create(
-                        $"child-[:CONNECTED {{likes: {relation.Likes}, comments: {relation.Comments}, follower: {relation.Follower}}}]->parent")
+                        $"child-[:CONNECTED {{likes: {relation.Likes}, comments: {relation.Comments}, follower: {relation.Follower}, date: {relation.DateOfParsing}}}]->parent")
                     .ExecuteWithoutResults();
             }
         }
@@ -50,20 +54,52 @@ namespace InfluencerInstaParser.Database
             {
                 graphClient.Cypher
                     .Match("(user:User)", "(location:Location)")
-                    .Where((ModelUser user) => user.Username == relation.Child)
+                    .Where((ModelUser user) =>
+                        user.Username == relation.Child && user.DateOfParsing == relation.DateOfParsing)
                     .AndWhere(
-                        (Location location) => location.Name == relation.Name && location.Owner == relation.Parent)
+                        (Location location) => location.Name == relation.Name && location.Owner == relation.Parent &&
+                                               location.DateOfParsing == relation.DateOfParsing)
                     .Create(
-                        $"user-[:VISITED {{count: {relation.Count}}}]->location")
+                        $"user-[:VISITED {{count: {relation.Count}, date: {relation.DateOfParsing}}}]->location")
                     .ExecuteWithoutResults();
             }
         }
 
-        public static List<ModelUser> GetListOfInfluencers(GraphClient graphClient, string targetUsername)
+        public static List<DateTime> GetListOfDatesOfProcessing(GraphClient graphClient, string targetUsername)
         {
+            var dates = graphClient.Cypher
+                .Match("(user:User)")
+                .Where((ModelUser user) => user.Username == targetUsername)
+                .Return(user => new
+                {
+                    Date = DateTime.Parse(user.As<ModelUser>().DateOfParsing)
+                })
+                .Results;
+            var listOfDates = dates.ToList();
+            return listOfDates.Count == 0 ? new List<DateTime>() : (from date in listOfDates select date.Date).ToList();
+        }
+
+        public static DateTime GetLastDateOfProcessing(GraphClient graphClient, string targetUsername)
+        {
+            var dates = GetListOfDatesOfProcessing(graphClient, targetUsername);
+            var currentDateValue = DateTime.MinValue;
+            if (dates.Count == 0) return currentDateValue;
+            foreach (var date in dates)
+            {
+                if (DateTime.Compare(date, currentDateValue) > 0) currentDateValue = date;
+            }
+
+            return currentDateValue;
+        }
+
+        public static List<ModelUser> GetListOfInfluencers(GraphClient graphClient, DateTime dateOfParsing,
+            string targetUsername)
+        {
+            var date = dateOfParsing.ToString(CultureInfo.InvariantCulture);
             var users = graphClient.Cypher
                 .Match("(user:User)")
-                .Where((ModelUser user) => user.IsInfluencer == true && user.Parents.Contains(targetUsername))
+                // ReSharper disable once RedundantBoolCompare
+                .Where((ModelUser user) => user.IsInfluencer == true && user.DateOfParsing == date)
                 .Return(user => new
                 {
                     User = user.As<ModelUser>()
@@ -72,11 +108,13 @@ namespace InfluencerInstaParser.Database
             return (from user in users where user.User.Parents.Contains(targetUsername) select user.User).ToList();
         }
 
-        public static List<Location> GetListOfLocationsFromTarget(GraphClient graphClient, string targetUsername)
+        public static List<Location> GetListOfLocationsFromTarget(GraphClient graphClient, DateTime dateOfParsing,
+            string targetUsername)
         {
+            var date = dateOfParsing.ToString(CultureInfo.InvariantCulture);
             var locations = graphClient.Cypher
                 .Match("(location:Location)")
-                .Where((Location location) => location.Owner == targetUsername)
+                .Where((Location location) => location.Owner == targetUsername && location.DateOfParsing == date)
                 .Return(location => new
                 {
                     Loc = location.As<Location>()
@@ -84,5 +122,11 @@ namespace InfluencerInstaParser.Database
                 .Results;
             return (from location in locations select location.Loc).ToList();
         }
+
+//        public static List<KeyValuePair<ModelUser, int>> GetRankedListOfInfluencers(GraphClient graphClient,
+//            DateTime dateOfParsing, string targetUsername)
+//        {
+//            var rankedInfluencers = graphClient
+//        }
     }
 }
