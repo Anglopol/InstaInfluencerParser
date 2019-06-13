4072d96f00d4349050fc06c5c2ad18bcc6f3ed19
anglopol <vertvery@gmail.com>
2019-06-13 17:43:47 +0300

    fix

--- InfluencerInstaParser/Database/ModelView/Analysis.cs
+++ InfluencerInstaParser/Database/ModelView/Analysis.cs
@@ -5,7 +5,7 @@ namespace InfluencerInstaParser.Database.ModelView
     public class Analysis
     {
         [JsonProperty("name")] public string TargetName { get; set; }
-        [JsonProperty("name")] public string Date { get; set; }
-        [JsonProperty("name")] public string Id { get; set; }
+        [JsonProperty("date")] public string Date { get; set; }
+        [JsonProperty("id")] public string Id { get; set; }
     }
 }
