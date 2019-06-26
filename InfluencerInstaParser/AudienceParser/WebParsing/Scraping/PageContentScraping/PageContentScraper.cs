e03cfc24d116f0af7b2e37bfa5f1a1eaee0306a5
Ivan_ <vertvery@gmail.com>
2019-06-26 16:34:03 +0300

    New PageContentScraper class

---
+++ InfluencerInstaParser/AudienceParser/WebParsing/Scraping/PageContentScraping/PageContentScraper.cs
@@ -0,0 +1,98 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace InfluencerInstaParser.AudienceParser.WebParsing.Scraping.PageContentScraping
+{
+    public class PageContentScraper : IInstagramPageContentScraper
+    {
+        public double GetLatitudeFromLocationPage(string locationPageContent)
+        {
+            return double.Parse(
+                Regex.Match(locationPageContent, "location:latitude\" content=\"[^\"]*")
+                    .ToString().Split("\"")[2]);        }
+
+        public double GetLongitudeFromLocationPage(string locationPageContent)
+        {
+            return double.Parse(
+                Regex.Match(locationPageContent, "location:longitude\" content=\"[^\"]*")
+                    .ToString().Split("\"")[2]);
+        }
+
+        public bool IsPostVideo(string postPageContent)
+        {
+            return postPageContent.Contains("\"is_video\":true");
+        }
+
+        public IEnumerable<string> GetUsernamesFromPostPage(string postPageContent)
+        {
+            return Regex.Matches(postPageContent, "username\".{2}[^\"]*").Select(match => match.Value.ToString()
+                .Split(":")[1].Remove(0, 1)).ToList();
+        }
+
+        public string GetRhxGisParameterFromUserPage(string pageContent)
+        {
+            return Regex.Matches(pageContent, "rhx_gis.{3}[^\"]*")[0].ToString()
+                .Split(":")[1].Remove(0, 1);
+        }
+
+        public string GetEndOfCursorFromUserPage(string pageContent)
+        {
+            if (IsContentHasNextPage(pageContent))
+                return Regex.Match(pageContent,
+                        "\"has_next_page\":true,\"end_cursor.{3}[^\"]*").ToString()
+                    .Split(":")[2].Remove(0, 1);
+
+            return "";
+        }
+
+        public ulong GetUserIdFromUserPage(string pageContent)
+        {
+            return ulong.Parse(Regex.Match(pageContent, "owner\":{\"id.{3}[^\"]*").ToString()
+                .Split(":")[2].Remove(0, 1));
+        }
+
+        public int GetNumberOfSubscribersFromUserPage(string pageContent)
+        {
+            var number = int.Parse(Regex.Match(pageContent, "edge_followed_by\":{\"count\":[^}]*")
+                .ToString().Split(":")[2]);
+            return number;
+        }
+
+        public int GetNumberOfFollowingFromUserPage(string pageContent)
+        {
+            var number = int.Parse(Regex.Match(pageContent, "edge_follow\":{\"count\":[^}]*")
+                .ToString().Split(":")[2]);
+            return number;
+        }
+
+        public IEnumerable<string> GetShortCodesFromUserPage(string pageContent)
+        {
+            return Regex.Matches(pageContent, "shortcode.{3}[^\"]*").Select(match => match.Value.ToString()
+                .Split(":")[1].Remove(0, 1));
+        }
+
+        public IEnumerable<ulong> GetLocationsIdFromUserPage(string pageContent)
+        {
+            var listOfStrings = Regex.Matches(pageContent, "location\":[^,]*")
+                .Where(match => !match.Value.Contains("null")).Select(
+                    match => match.Value.ToString().Split("\"")[4]);
+            return from shortCode in listOfStrings select ulong.Parse(shortCode);
+        }
+
+        public bool IsUserPagePrivate(string pageContent)
+        {
+            return pageContent.Contains("\"is_private\":true");
+        }
+
+        public bool IsUserPageEmpty(string pageContent)
+        {
+            return pageContent.Contains("edge_owner_to_timeline_media\":{\"count\":0") || pageContent == "";
+        }
+
+        public bool IsContentHasNextPage(string pageContent)
+        {
+            return pageContent.Contains("\"has_next_page\":true");
+        }
+    }
+}
