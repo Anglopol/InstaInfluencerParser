e28fad9497731c753d56c16d14e37e760be2d293
Ivan_ <vertvery@gmail.com>
2019-07-03 20:11:18 +0300

    Instagram Parser class

--- InfluencerInstaParser/AudienceParser/InstagramParser.cs
+++ InfluencerInstaParser/AudienceParser/InstagramParser.cs
@@ -1,7 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using InfluencerInstaParser.AudienceParser.ResultOfParsing;
 using InfluencerInstaParser.AudienceParser.WebParsing.InstagramResponseParser;
+using InfluencerInstaParser.AudienceParser.WebParsing.InstagramResponseParser.LocationParsing;
+using InfluencerInstaParser.AudienceParser.WebParsing.InstagramResponseParser.PostParsing.CommentsParsing;
+using InfluencerInstaParser.AudienceParser.WebParsing.InstagramResponseParser.PostParsing.LikesParsing;
 using InfluencerInstaParser.AudienceParser.WebParsing.InstagramResponseParser.UserParsing;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -10,11 +15,14 @@ namespace InfluencerInstaParser.AudienceParser
     public class InstagramParser : IInstagramParser
     {
         private readonly IUserPageParser _userPageParser;
+        private readonly IServiceProvider _serviceProvider;
 
         public InstagramParser(IServiceProvider serviceProvider)
         {
+            _serviceProvider = serviceProvider;
             _userPageParser = serviceProvider.GetService<IUserPageParser>();
         }
+
         public IParsingResult ParseByUsername(string username)
         {
             var userPage = _userPageParser.GetUserPage(username);
@@ -30,10 +38,43 @@ namespace InfluencerInstaParser.AudienceParser
 
         private IParsingResult PostsProcessing(IEnumerable<Post> posts)
         {
-            foreach (var post in posts)
-            {
-                
-            }
+            var parsingResult = new ParsingResult();
+            var postProcessingTasks = posts.Select(post =>
+                Task.Factory.StartNew(() => SinglePostProcessing(post, parsingResult),
+                    TaskCreationOptions.LongRunning));
+            Task.WaitAll(postProcessingTasks.ToArray());
+
+            return parsingResult;
+        }
+
+        private void SinglePostProcessing(Post post, ParsingResult parsingResult)
+        {
+            parsingResult.Posts.Add(post);
+            
+            Task.Factory.StartNew(() => CommentsPostProcessing(post, parsingResult),
+                TaskCreationOptions.AttachedToParent);
+            Task.Factory.StartNew(() => LikesPostProcessing(post, parsingResult),
+                TaskCreationOptions.AttachedToParent);
+            Task.Factory.StartNew(() => LocationPostProcessing(post, parsingResult),
+                TaskCreationOptions.AttachedToParent);
+        }
+
+        private void CommentsPostProcessing(Post post, ParsingResult parsingResult)
+        {
+            var commentsParser = _serviceProvider.GetService<ICommentsParser>();
+            parsingResult.AddUsersFromComments(commentsParser.GetUsersFromComments(post));
+        }
+
+        private void LikesPostProcessing(Post post, ParsingResult parsingResult)
+        {
+            var likesParser = _serviceProvider.GetService<ILikesParser>();
+            parsingResult.AddUsersFromLikes(likesParser.GetUsersFromLikes(post));
+        }
+        private void LocationPostProcessing(Post post, ParsingResult parsingResult)
+        {
+            var locator = _serviceProvider.GetService<ILocator>();
+            var locationScrapResult = locator.GetLocatorScrapingResultByLocationId(post.LocationId);
+            parsingResult.ScrapedLocations.Add(locationScrapResult);
         }
     }
 }
