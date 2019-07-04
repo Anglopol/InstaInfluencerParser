41ab1f89d27cf45ab07eff09680d77e4d0cb78c0
Ivan_ <vertvery@gmail.com>
2019-07-04 17:53:52 +0300

    Изменения в классе парсинг резалт

--- InfluencerInstaParser/AudienceParser/ResultOfParsing/IParsingResult.cs
+++ InfluencerInstaParser/AudienceParser/ResultOfParsing/IParsingResult.cs
@@ -1,7 +1,16 @@
+using System.Collections.Generic;
+using InfluencerInstaParser.AudienceParser.UserCreating.ParsedUser;
+using InfluencerInstaParser.AudienceParser.WebParsing.InstagramResponseParser;
+using InfluencerInstaParser.AudienceParser.WebParsing.InstagramResponseParser.LocationParsing;
+
 namespace InfluencerInstaParser.AudienceParser.ResultOfParsing
 {
     public interface IParsingResult
     {
-        User CreateUser();
+        IUser CreateUser();
+        void AddUsersFromComments(IEnumerable<ParsedUserFromJson> users);
+        void AddUsersFromLikes(IEnumerable<ParsedUserFromJson> users);
+        void AddLocationScrapResult(LocatorScrapingResult scrapingResult);
+        void AddPost(Post post);
     }
 }
--- InfluencerInstaParser/AudienceParser/ResultOfParsing/ParsingResult.cs
+++ InfluencerInstaParser/AudienceParser/ResultOfParsing/ParsingResult.cs
@@ -1,31 +1,48 @@
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using InfluencerInstaParser.AudienceParser.UserCreating;
+using InfluencerInstaParser.AudienceParser.UserCreating.ParsedUser;
 using InfluencerInstaParser.AudienceParser.WebParsing.InstagramResponseParser;
 using InfluencerInstaParser.AudienceParser.WebParsing.InstagramResponseParser.LocationParsing;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace InfluencerInstaParser.AudienceParser.ResultOfParsing
 {
     public class ParsingResult : IParsingResult
     {
+        private const int NormOfAverageActivityForInfluencer = 1000;
+        private const int CommentCoefficient = 2;
+        
         public readonly ConcurrentBag<Post> Posts;
-        public ConcurrentBag<LocatorScrapingResult> ScrapedLocations;
-        public ConcurrentBag<ParsedUser> UsersFromLikes { get; }
-        public ConcurrentBag<ParsedUser> UsersFromComments { get; }
+        public readonly ConcurrentBag<LocatorScrapingResult> ScrapedLocations;
+        public ConcurrentBag<ParsedUserFromJson> UsersFromLikes { get; }
+        public ConcurrentBag<ParsedUserFromJson> UsersFromComments { get; }
+        private IServiceProvider _serviceProvider;
 
-        public ParsingResult()
+        public ParsingResult(IServiceProvider serviceProvider)
         {
+            _serviceProvider = serviceProvider;
             Posts = new ConcurrentBag<Post>();
             ScrapedLocations = new ConcurrentBag<LocatorScrapingResult>();
-            UsersFromLikes = new ConcurrentBag<ParsedUser>();
-            UsersFromComments = new ConcurrentBag<ParsedUser>();
+            UsersFromLikes = new ConcurrentBag<ParsedUserFromJson>();
+            UsersFromComments = new ConcurrentBag<ParsedUserFromJson>();
         }
 
-        public User CreateUser()
+        public IUser CreateUser()
         {
-            throw new System.NotImplementedException();
+            var userFactory = _serviceProvider.GetService<IUserFactory>();
+            if (Posts.IsEmpty) userFactory.CreateUser();
+            var username = GetUsername();
+            var userId = GetUserId();
+            var isInfluencer = IsInfluencer();
+            var newUser = userFactory.CreateUser(username, userId, isInfluencer, ScrapedLocations);
+            newUser.UsersFromLikes = UsersFromLikes;
+            newUser.UsersFromComments = UsersFromComments;
+            return newUser;
         }
 
-        public void AddUsersFromComments(IEnumerable<ParsedUser> users)
+        public void AddUsersFromComments(IEnumerable<ParsedUserFromJson> users)
         {
             foreach (var parsedUser in users)
             {
@@ -33,12 +50,52 @@ namespace InfluencerInstaParser.AudienceParser.ResultOfParsing
             }
         }
 
-        public void AddUsersFromLikes(IEnumerable<ParsedUser> users)
+        public void AddUsersFromLikes(IEnumerable<ParsedUserFromJson> users)
         {
             foreach (var parsedUser in users)
             {
                 UsersFromLikes.Add(parsedUser);
             }
+        }
+
+        public void AddLocationScrapResult(LocatorScrapingResult scrapingResult)
+        {
+            ScrapedLocations.Add(scrapingResult);
+        }
+
+        public void AddPost(Post post)
+        {
+            Posts.Add(post);
+        }
+
+        private bool IsInfluencer()
+        {
+            var numberOfPosts = Posts.Count;
+            var countOfActivity = GetCountOfActivity();
+            return countOfActivity / numberOfPosts >= NormOfAverageActivityForInfluencer;
+        }
+
+        private int GetCountOfActivity()
+        {
+            var count = 0;
+            foreach (var post in Posts)
+            {
+                count += post.Comments * CommentCoefficient + post.Likes;
+            }
+
+            return count;
+        }
+
+        private string GetUsername()
+        {
+            Posts.TryPeek(out var randomPost);
+            return randomPost.Owner;
+        }
+
+        private ulong GetUserId()
+        {
+            Posts.TryPeek(out var randomPost);
+            return randomPost.OwnerId;
         }
 
     }
